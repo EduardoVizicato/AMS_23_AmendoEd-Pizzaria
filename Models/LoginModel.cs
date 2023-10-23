f5f7e7b354b487655d65edf0e8c8df6674315e38
unknown <igorbaroni77@gmail.com>
2023-10-23 14:18:02 -0300

    InitialProject

---
+++ Models/LoginModel.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace AMS23_Carousel.Models
+{
+    public class LoginModel
+    {
+        public string Email {get; set;}
+
+        public string Password {get; set;}
+    }
+}
